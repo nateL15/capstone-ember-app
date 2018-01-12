8aa0ebaf299da1a87b37b5d861466f2ceb85e5be
nateL15 <nathanlass@gmail.com>
2018-01-11 19:21:06 -0500

    Initial Commit

---
+++ app/routes/change-password.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import RSVP from 'rsvp';
+
+export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
+
+  model () {
+    return RSVP.Promise.resolve({})
+  },
+
+  actions: {
+    changePassword (passwords) {
+      this.get('auth').changePassword(passwords)
+      .then(() => this.get('auth').signOut())
+      .then(() => this.transitionTo('sign-in'))
+      .then(() => {
+        this.get('flashMessages')
+        .success('Successfully changed your password!');
+      })
+      .then(() => {
+        this.get('flashMessages').warning('You have been signed out.');
+      })
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem. Please try again.');
+      });
+    },
+  },
+});
